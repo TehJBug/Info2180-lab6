fc825bc1215382e419023bc8bed109ca09d571cb
Jason Brown <jason.brown08@mymona.uwi.edu>
2016-11-10 03:19:41 +0000

    XML parsing error

--- request.php
+++ request.php
@@ -15,38 +15,39 @@ $definition = [
 ];
 
 $xmlData = '<?xml version="1.0" encoding="UTF-8"?>
-
-<entries>
-    <definition name="definition" author="John">
-        A statement of the exact meaning of a word, especially in a dictionary.
-    </defintion>
-    <definition name="bar" author="Jason">
-        A place that sells alcholic beverages.
-    </defintion>
-    <definition name="ajax" author="Jason">
-        Technique which involves the use of javascript and xml (or JSON)
-    </defintion>
-    <definition name="html" author="Jason">
-        The standard markup language for creating web pages and web applications.
-    </definition>
-    <definition name="css" author="Jason">
-        A style sheet language used for describing the presentation of a document written in a markup language.
-    </definition>
-    <definition name="javascript" author="Jason">
-        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
-    </definition>
-    <definition name="php" author="Jason">
-        A server-side scripting language, and a powerful tool for making dynamic and interactive websites
-    </definition>';
+            <entries>
+                <definition name="definition" author="John">
+                    A statement of the exact meaning of a word, especially in a dictionary.
+                </defintion>
+                <definition name="bar" author="Jason">
+                    A place that sells alcholic beverages.
+                </defintion>
+                <definition name="ajax" author="Jason">
+                    Technique which involves the use of javascript and xml (or JSON)
+                </defintion>
+                <definition name="html" author="Jason">
+                    The standard markup language for creating web pages and web applications.
+                </definition>
+                <definition name="css" author="Jason">
+                    A style sheet language used for describing the presentation of a document written in a markup language.
+                </definition>
+                <definition name="javascript" author="Jason">
+                    A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+                </definition>
+                <definition name="php" author="Jason">
+                    A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+                </definition>
+            </entries>';
     
-    header('Content-Type: text/xml');
-    $xmlOutput = new SimpleXMLElement($xmldata);    
+    
 
-if(all == false)
+if($all == "false" )
 {
     print "<h3>" . strtoupper($query) . "</h3>";
     print "<p>" . $definition[$query] . "</p>";
 }
 else {
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);    
     echo $xmlOutput->asXML();
 }
